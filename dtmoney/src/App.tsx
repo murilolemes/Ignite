5110c9b4c255067335393daeffd94877cd7fb1b0
Murilo Lemes <murilo.lemes.mhl@gmail.com>
2022-10-09 18:57:30 -0300

    Adicionando projeto da aula do Ignite

---
+++ dtmoney/src/App.tsx
@@ -0,0 +1,32 @@
+import { useState } from 'react';
+import Modal from 'react-modal';
+
+import { Dashboard } from "./components/Dashboard";
+import { Header } from "./components/Header";
+import { NewTransactionModal } from './components/NewTransactionModal';
+import { TransactionsProvider } from './TransactionsContext';
+
+import { GlobalStyle } from "./styles/global";
+
+export function App() {
+  const [isNewTransactionModalOpen, setIsNewTransactionModalOpen] = useState(false)
+
+  function handleOpenNewTransactionModal() {
+    setIsNewTransactionModalOpen(true);
+  }
+
+  function handleCloseNewTransactionModal() {
+    setIsNewTransactionModalOpen(false)
+  }
+
+  Modal.setAppElement('#root');
+
+  return (
+    <TransactionsProvider>
+      <Header onOpenNewTransactionModal={handleOpenNewTransactionModal} />
+      <Dashboard />
+      <NewTransactionModal isOpen={isNewTransactionModalOpen} onRequestClose={handleCloseNewTransactionModal} />
+      <GlobalStyle />
+    </TransactionsProvider>
+  );
+}
